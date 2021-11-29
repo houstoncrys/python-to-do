c14169d257ed45f8ad633b0664eb5abbed1bf264
flomflam <32689713+flomflam@users.noreply.github.com>
2021-11-29 19:33:16 +0000

    add file as example

--- todo.py
+++ todo.py
@@ -1,1 +1,34 @@
+myTodo = ["Go grocery shopping","Make dinner","Do the dishes"]
 
+print('My TODO List')
+
+print(f'{len(myTodo)} thing(s) left to do')
+
+for task in myTodo:
+  print(task)
+
+print('What task would you like to add?')
+
+add = input()
+
+myTodo.append(add)
+
+print('My TODO List')
+
+print(f'{len(myTodo)} thing(s) left to do')
+
+for task in myTodo:
+  print(task)
+
+print('What task did you complete?')
+
+complete = input()
+
+myTodo.remove(complete)
+
+print('My TODO List')
+
+print(f'{len(myTodo)} thing(s) left to do')
+
+for task in myTodo:
+  print(task)
