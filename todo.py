b4e4860b29a4bdd9590dd30b4962f16ab2a89699
houstoncrys <95242881+houstoncrys@users.noreply.github.com>
2021-12-01 14:24:24 +0000

    updates for class

--- todo.py
+++ todo.py
@@ -1,34 +1,33 @@
-myTodo = ["Go grocery shopping","Make dinner","Do the dishes"]
+#CREATE TO DO LIST
+my_to_do = ["Go grocery shopping","Make dinner","Do the dishes"]
 
+#PRINT OUT STRING 'MY TODO LIST'
 print('My TODO List')
-
-print(f'{len(myTodo)} thing(s) left to do')
-
-for task in myTodo:
+#PRINT OUT HOW MANY ITEMS I HAVE LEFT ON MY TODO LIST
+print(f'{len(my_to_do)} thing(s) left to do')
+#FOR LOOP THAT PRINTS OUT EACH ITEM IN MY TODO LIST
+for task in my_to_do:
   print(task)
 
-print('What task would you like to add?')
+# #ADD TO LIST
+# print('What task would you like to add?')
+# add = input()
+# my_to_do.append(add)
 
-add = input()
+# #CODE EXECUTES FROM TOP TO BOTTOM
+# #WE NEED TO DUPLICATE THIS PIECE OF CODE TO PRINT OUT OUR LIST AGAIN
+# print('My TODO List')
+# print(f'{len(my_to_do)} thing(s) left to do')
+# for task in my_to_do:
+#   print(task)
 
-myTodo.append(add)
+# #REMOVE FROM LIST
+# print('What task did you complete?')
+# complete = input()
+# my_to_do.remove(complete)
 
-print('My TODO List')
-
-print(f'{len(myTodo)} thing(s) left to do')
-
-for task in myTodo:
-  print(task)
-
-print('What task did you complete?')
-
-complete = input()
-
-myTodo.remove(complete)
-
-print('My TODO List')
-
-print(f'{len(myTodo)} thing(s) left to do')
-
-for task in myTodo:
-  print(task)
+# #DUPLICATE THIS PIECE OF CODE AGAIN
+# print('My TODO List')
+# print(f'{len(my_to_do)} thing(s) left to do')
+# for task in my_to_do:
+#   print(task)
